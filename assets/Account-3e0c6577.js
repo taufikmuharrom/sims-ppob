f0aedc3119ea95bb8a0426dad7b5b5305a2b7486
taufikmuharrom <taufikmuharromdev@gmail.com>
2023-09-09 00:02:03 +0700

    Updates

---
+++ assets/Account-3e0c6577.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-821e7249.js";const r=()=>t.jsx("div",{children:"Account"});export{r as default};
---
+++ assets/NotFound-03b0d982.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-821e7249.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
